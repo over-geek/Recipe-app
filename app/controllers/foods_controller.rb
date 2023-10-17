9e2c67668a3932f8cf63ecda619b8c497b4dd9c0
over-geek <kenagy100@gmail.com>
2023-10-17 22:27:29 +0000

    Add food controller actions

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,39 @@
+class FoodsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :set_food, only: [:show, :destroy]
+	# load_and_authorize_resource except: [:show]
+
+	def index
+		@foods = current_user.foods
+	end
+
+	def new
+		@food = Food.new
+	end
+
+	def create
+		@food = current_user.foods.build(food_params)
+		if @food.save
+			redirect_to foods_path, notice: 'Food item added successfully.'
+		else
+			render :new
+		end
+	end
+
+	def show
+		if @food.destroy
+			redirect_to foods_path, notice: 'Food item deleted.'
+		else
+			redirect_to foods_path, alert: 'Failed to delete food item.'
+		end
+  end
+
+	def set_food
+		@food = current_user.foods.find(params[:id])
+	end
+
+	def food_params
+		params.require(:food).permit(:name, :quantity, :price)
+	end
+
+end
