81cf5792303babc8be173a2914be439d72d92410
francksefu <francksefu1998@gmail.com>
2023-10-19 00:45:34 +0200

    Correct linters

--- app/controllers/general_list_controller.rb
+++ app/controllers/general_list_controller.rb
@@ -4,7 +4,8 @@ class GeneralListController < ApplicationController
   def index
     @tot_price = 0
     @tot_count = 0
-    @foods = Food.left_outer_joins(:recipe_foods).where(recipe_foods: { food_id: nil }, foods: { user_id: current_user.id})
+    @foods = Food.left_outer_joins(:recipe_foods).where(recipe_foods: { food_id: nil },
+                                                        foods: { user_id: current_user.id })
     @foods.each do |t|
       @tot_price += t.price
       @tot_count += 1
