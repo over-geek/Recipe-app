2f6a551191f3e8cd889cc19c85b995ee5dd6c5cc
over-geek <kenagy100@gmail.com>
2023-10-19 18:58:50 +0000

    Add public recipes to navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :foods, dependent: :destroy
   accepts_nested_attributes_for :foods
   has_many :recipes, dependent: :destroy
