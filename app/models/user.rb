1c40d85f153f8b39516477a638f1c40341ca7f65
over-geek <kenagy100@gmail.com>
2023-10-19 18:45:03 +0000

    Add name attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-  has_many :foods
-  has_many :recipes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :foods, dependent: :destroy
+  accepts_nested_attributes_for :foods
+  has_many :recipes, dependent: :destroy
+  validates :name, presence: true
 end
