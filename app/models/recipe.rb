c0fe9e3c272278c64a2c5e989b80059ca3bd1c36
over-geek <kenagy100@gmail.com>
2023-10-19 18:46:53 +0000

    Add calculate_total_amount method to use in public recipes page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,16 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods, dependent: :destroy
 
+  def calculate_total_amount
+    total_amount = 0
+    recipe_foods.each do |recipe_food|
+      total_amount += recipe_food.food.price * recipe_food.quantity
+    end
+    total_amount
+  end
+
+  scope :public_recipes, -> { where(public: true) }
+
   def public?
     is_public
   end
